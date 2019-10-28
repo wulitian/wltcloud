f240481edb8f95eefe2242ea06e4e02ea343b225
13344 <1334416530@qq.com>
2019-10-28 13:43:21 +0800

    zipkin

---
+++ wltcloud-zipkin/src/main/java/com/wltcloud/zipkin/ZipKinApplication.java
@@ -0,0 +1,15 @@
+package com.wltcloud.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import zipkin.server.internal.EnableZipkinServer;
+/*@EnableZipkinServer开启服务链路踪*/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableZipkinServer
+public class ZipKinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipKinApplication.class, args);
+    }
+}
