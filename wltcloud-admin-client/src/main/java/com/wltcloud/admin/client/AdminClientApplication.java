b1ef9095a3675e0f62f5811d4341c92ab03896c1
13344 <1334416530@qq.com>
2019-10-28 14:39:22 +0800

    集成spring admin

---
+++ wltcloud-admin-client/src/main/java/com/wltcloud/admin/client/AdminClientApplication.java
@@ -0,0 +1,13 @@
+package com.wltcloud.admin.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class AdminClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminClientApplication.class, args);
+    }
+}
