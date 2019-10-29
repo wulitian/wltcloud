5d650bbfc2847cd9894e1f4ea38a2db959409065
13344 <1334416530@qq.com>
2019-10-29 14:58:51 +0800

    集成nacos提供者客户端feign(wltcloud-feign与eurka整合时配置选为github不太方便开发)

---
+++ wltcloud-alibaba-nacos-consumer-feign/src/main/java/com/wltcloud/alibaba/nacos/consumer/feign/service/EchoService.java
@@ -0,0 +1,12 @@
+package com.wltcloud.alibaba.nacos.consumer.feign.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(value = "wltcloud-alibaba-nacos-provider")
+public interface EchoService {
+
+    @GetMapping(value = "/echo/{message}")
+    String echo(@PathVariable("message") String message);
+}
