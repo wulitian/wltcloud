ebe76058493702725923e9d6e9c48119ad0b7939
13344 <1334416530@qq.com>
2019-10-31 08:44:25 +0800

    将配置文件上传nacos

---
+++ wltcloud-alibaba-nacos-provider/src/main/java/com/wltcloud/alibaba/nacos/provider/controller/ProviderController.java
@@ -0,0 +1,19 @@
+package com.wltcloud.alibaba.nacos.provider.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProviderController {
+    // 注入配置文件上下文
+    @Autowired
+    private ConfigurableApplicationContext applicationContext;
+
+    // 从上下文中读取配置
+    @GetMapping(value = "/hi")
+    public String sayHi() {
+        return "Hello " + applicationContext.getEnvironment().getProperty("server.port");
+    }
+}
