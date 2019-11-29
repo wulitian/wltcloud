1eef4238c99a6bc6d8dc3e955803e6178263c72e
13344 <1334416530@qq.com>
2019-11-29 18:24:57 +0800

    oauth2

---
+++ wltcloud-alibaba-mybatis/src/main/java/com/wltcloud/mybatis/entity/Person.java
@@ -0,0 +1,55 @@
+package com.wltcloud.mybatis.entity;
+
+import javax.persistence.*;
+
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String sex;
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return sex
+     */
+    public String getSex() {
+        return sex;
+    }
+
+    /**
+     * @param sex
+     */
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+}
---
+++ wltcloud-alibaba-mybatis/src/main/java/com/wltcloud/mybatis/mapper/PersonMapper.java
@@ -0,0 +1,8 @@
+package com.wltcloud.mybatis.mapper;
+
+import com.wltcloud.mybatis.entity.Person;
+import com.wltcloud.mybatis.utils.MyMapper;
+
+public interface PersonMapper extends MyMapper<Person> {
+
+}
